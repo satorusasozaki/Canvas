d5f58e09d465efb26a75849c2206551b59e065b2
satorusasozaki <sasozakisatoru@gmail.com>
2016-11-03 11:21:22 -0700

    Add animation when pan recognizer begin

--- CanvasViewController.swift
+++ CanvasViewController.swift
@@ -14,40 +14,42 @@ class CanvasViewController: UIViewController {
     var trayDefaultCenter = CGPoint(x: 187, y: 525)
     var trayCenterWhenOpen = CGPoint(x: 187.5, y: 527.5)
     //var trayCenterWhenClose = CGPoint(x: 187.5, y: 767.5)
-    var trayCenterWhenClose = CGPoint(x: 187.5, y: 666.0)
+    var trayCenterWhenClose = CGPoint(x: 187.5, y: 765.0)
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    // How to do animation like control center? 
+    // Move view when pan is ended but it is not smooth meaning it stops once when pan ended and start animating
     @IBAction func onTrayPanGesture(_ sender: UIPanGestureRecognizer) {
-        let point = sender.location(in: self.view)
-        print("original center: \(sender.view?.center)")
+        let tappedPoint = sender.location(in: self.view)
         if sender.state == UIGestureRecognizerState.began {
             trayOriginalCenter = sender.view?.center
-            print("Gesture began at: \(point)")
             if sender.velocity(in: self.view!).y > 0 {
                 // moving down
-                print("Moving down with velocity: \(sender.velocity(in: self.view!).y)")
-                UIView.animate(withDuration: 1.5, animations: {
-                    //sender.view?.center = self.trayCenterWhenClose
-                    print("Center: \(sender.view?.center)")
+                //print("Moving down with velocity: \(sender.velocity(in: self.view!).y)")
+                
+                UIView.animate(withDuration: 0.4, delay: 0, options: [], animations: {
+                    sender.view?.center = self.trayCenterWhenClose
+                }, completion: { ( animationIsFinished :Bool) in
+                    
                 })
             } else {
                 // moving up
-                print("Moving up with velocity: \(sender.velocity(in: self.view!).y)")
-                UIView.animate(withDuration: 1.5, animations: {
+                //print("Moving up with velocity: \(sender.velocity(in: self.view!).y)")
+                UIView.animate(withDuration: 0.4, delay: 0, options: [], animations: {
                     sender.view?.center = self.trayCenterWhenOpen
+                }, completion: { ( animationIsFinished :Bool) in
+                    
                 })
             }
         } else if sender.state == UIGestureRecognizerState.changed {
-            print("Gesture changed at: \(point)")
             let yOffset = sender.translation(in: self.view).y
-            sender.view?.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + yOffset)
+            //sender.view?.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + yOffset)
         } else if sender.state == UIGestureRecognizerState.ended {
-            print("Gesture ended at: \(point)")
-            print("Tray center: \(sender.view?.center)")
+            //print("Tray center: \(sender.view?.center)")
+
         }
     }
 
